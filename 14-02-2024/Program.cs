ac122eb63e9d1a52b85ffb5f93b81d1963fb6393
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-14 14:10:12 +1300

    Update Program.cs

--- 14-02-2024/Program.cs
+++ 14-02-2024/Program.cs
@@ -3,4 +3,20 @@
 // and a bit of input from the user
 
 using System;
+using System.Reflection.Metadata.Ecma335;
 
+class Test { 
+    // simple function which says welcome
+    public static void welcome() {
+        Console.WriteLine("Welcome");
+    } // end welcome
+    
+    // this Main function is the entry-point in this file
+    public static void Main(string[] args) { 
+        Console.WriteLine("welcome is a user-defined function");
+        Console.WriteLine("_______________________________");
+        welcome(); 
+
+        Console.WriteLine("\n");
+    } // end Main
+} // end class
