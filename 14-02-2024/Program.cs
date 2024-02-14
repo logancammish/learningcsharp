d65279d9a4d996f85811b0e855465a04c2c0513f
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-14 14:26:54 +1300

    Update Program.cs

--- 14-02-2024/Program.cs
+++ 14-02-2024/Program.cs
@@ -3,13 +3,22 @@
 // and a bit of input from the user
 
 using System;
-using System.Reflection.Metadata.Ecma335;
+using System.Net;
 
 class Test { 
     // simple function which says welcome
     public static void welcome() {
         Console.WriteLine("Welcome");
     } // end welcome
+
+    public static void signature() { 
+        Console.WriteLine("-----wider---------");
+    } // end signature
+
+    //here is a function that takes a parameter //level 2
+    public static void HaveANice(string name) { 
+        Console.WriteLine("Have a nice day, " + name + "!");
+    } // end have a nice function
     
     // this Main function is the entry-point in this file
     public static void Main(string[] args) { 
@@ -18,5 +27,12 @@ class Test {
         welcome(); 
 
         Console.WriteLine("\n");
+
+        signature();
+
+        string str1; 
+        Console.Write("What is your name? "); 
+        str1 = Console.ReadLine(); 
+        HaveANice(str1);
     } // end Main
 } // end class
