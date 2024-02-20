cde687679630d1a6fd7073ffe2f750bb8370db27
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2024-02-21 08:42:27 +1300

    Create Program.cs

---
+++ 19-02-2024/Program.cs
@@ -0,0 +1,20 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() { 
+        return 40;
+    }
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutes) {
+        return 40 - minutes;
+    }
+    
+    // TODO: define the 'PreparationTimeInMinutes()' method
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+
+    public static void Main(string[] args) {
+        Console.WriteLine("hello");
+    }
+}
